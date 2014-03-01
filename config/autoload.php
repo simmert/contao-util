52e8b9ec13d945e748aebfe7ad9c375649cf5c6c
André Simmert <andre@twogermans.com>
2014-03-01 17:44:07 +0100

    Added autoload.php

---
+++ config/autoload.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Util
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+	'Util',
+));
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Lib
+	'Util\ContentElement'   => 'system/modules/util/lib/ContentElement.php',
+	'Util\FragmentTemplate' => 'system/modules/util/lib/FragmentTemplate.php',
+	'Util\CurrencyHelper'   => 'system/modules/util/lib/Helper/CurrencyHelper.php',
+	'Util\DcaHelper'        => 'system/modules/util/lib/Helper/DcaHelper.php',
+	'Util\FormHelper'       => 'system/modules/util/lib/Helper/FormHelper.php',
+	'Util\FragmentHelper'   => 'system/modules/util/lib/Helper/FragmentHelper.php',
+	'Util\GeneralHelper'    => 'system/modules/util/lib/Helper/GeneralHelper.php',
+	'Util\Model'            => 'system/modules/util/lib/Model.php',
+
+	// Widgets
+	'Util\Legend'           => 'system/modules/util/widgets/Legend.php',
+	'Util\ParentValue'      => 'system/modules/util/widgets/ParentValue.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'be_widget_legend'      => 'system/modules/util/templates/backend',
+	'be_widget_parentvalue' => 'system/modules/util/templates/backend',
+));
