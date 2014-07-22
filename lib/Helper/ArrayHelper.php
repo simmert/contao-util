eeab161fc1557a25c7c90a2e64b0e9e9000fd859
André Simmert <andre@priorist.com>
2014-07-22 17:00:20 +0200

    Added ArrayHelper::insert() and insertBefore()

--- lib/Helper/ArrayHelper.php
+++ lib/Helper/ArrayHelper.php
@@ -14,10 +14,22 @@ class ArrayHelper
 {
     public static function insertAfter(array &$baseArray, $key, array $insertArray)
     {
+        self::insert($baseArray, $key, $insertArray, 1);
+    }
+    
+    
+    public static function insertBefore(array &$baseArray, $key, array $insertArray)
+    {
+        self::insert($baseArray, $key, $insertArray, 0);
+    }
+    
+    
+    public static function insert(array &$baseArray, $key, array $insertArray, $relPos=1)
+    {
         $keys = array_keys($baseArray);
         $keyPos = array_search($key, $keys);
     
-        $insertPos = $keyPos === false ? count($baseArray) - 1 : $keyPos + 1;
+        $insertPos = $keyPos === false ? count($baseArray) - 1 : $keyPos + $relPos;
     
         array_insert($baseArray, $insertPos, $insertArray);
     }
