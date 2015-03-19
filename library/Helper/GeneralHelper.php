4a07fd5689b4bb7e56f08037612d84bb6713661a
André Simmert <andre@priorist.com>
2015-03-19 13:17:06 +0100

    Added GeneralHelper::generateRandomString()

--- library/Helper/GeneralHelper.php
+++ library/Helper/GeneralHelper.php
@@ -183,4 +183,17 @@ class GeneralHelper
             }
         }
     }
+    
+    
+    public static function generateRandomString($length=16)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i=0; $i<$length; $i++) {
+            $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
