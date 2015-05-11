5dadc51f3fb5c6d6cf209dcd53eef320ce72dd6d
André Simmert <andre@priorist.com>
2015-05-11 02:37:50 +0200

    Added StringHelper::sanitizeForFileSystem().

--- library/Helper/StringHelper.php
+++ library/Helper/StringHelper.php
@@ -18,4 +18,15 @@ class StringHelper
         // search forward starting from end minus needle length characters
         return $needle === '' || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
     }
+    
+    
+    public static function sanitizeForFileSystem($string)
+    {
+        // Remove anything which isn't a word, whitespace, number
+        // or any of the following caracters -_~[]().
+        $string = preg_replace("([^\w\s\d\-_~\[\]\(\).])", '', $string);
+        
+        // Remove any runs of periods
+        return preg_replace("([\.]{2,})", '', $string);
+    }
 }
